6ec9542e3020fec6e8731c3f56d8535883472b21
Kevin Kimaru Chege <kevinkimaru99@gmail.com>
2019-11-02 16:36:09 +0300

    config: create user

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -19,10 +19,9 @@ module.exports = {
 
   test: {
     database: 'books_test',
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: 'kevin',
+    password: null,
+    host: '127.0.0.1',
     dialect: 'postgres'
   },
 
